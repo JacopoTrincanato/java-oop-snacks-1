4e1026cdbbad28a592ed4b9c28093651be52850c
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-17 17:57:25 +0100

    create Studente class and its constructor

---
+++ java-oop-snacks-1/src/snack1/Studente.java
@@ -0,0 +1,16 @@
+package snack1;
+
+public class Studente {
+
+    // attributi
+    public String nome;
+    public String cognome;
+    public String eta;
+
+    // costruttore
+    Studente(String nome, String cognome, String eta) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+}
